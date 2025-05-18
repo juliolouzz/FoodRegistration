e971cc97ad206d04fb482fe0ab454aace4cc665e
juliolouzz <jlouz.dev@gmail.com>
2025-05-18 14:34:26 +0100

    first commit

---
+++ src/main/java/dev/juliolouzz/foodregistration/FoodRegistrationApplication.java
@@ -0,0 +1,13 @@
+package dev.juliolouzz.foodregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodRegistrationApplication.class, args);
+    }
+
+}
