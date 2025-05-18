a1fe3af63a046d6529578204787b75cc137e5abb
juliolouzz <jlouz.dev@gmail.com>
2025-05-18 17:33:35 +0100

    feat: add Controller with first route /food_api and an @GetMapping to /welcome

---
+++ src/main/java/dev/juliolouzz/foodregistration/Controller.java
@@ -0,0 +1,15 @@
+package dev.juliolouzz.foodregistration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/food_api")
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String Welcome() {
+        return "Welcome";
+    }
+}
