af4f22abdc6a0427606bb9599f6d23e880938340
juliolouzz <jlouz.dev@gmail.com>
2025-08-17 11:51:01 +0100

    chore: add version of DTO by entity and map manually in FoodMapper the path entity x DTO

--- src/main/java/dev/juliolouzz/foodregistration/Foods/FoodDTO.java
+++ src/main/java/dev/juliolouzz/foodregistration/Foods/FoodDTO.java
@@ -1,4 +1,24 @@
 package dev.juliolouzz.foodregistration.Foods;
 
+
+import dev.juliolouzz.foodregistration.Meals.MealsModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// It's a copy of the FoodModel, but without direct access to the database
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FoodDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Integer kcal;
+    private Integer quantity;
+    private String foodType;
+    private String imgUrl;
+    private MealsModel meals;
 }
--- src/main/java/dev/juliolouzz/foodregistration/Foods/FoodMapper.java
+++ src/main/java/dev/juliolouzz/foodregistration/Foods/FoodMapper.java
@@ -1,4 +1,34 @@
 package dev.juliolouzz.foodregistration.Foods;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class FoodMapper {
+
+    public FoodModel map(FoodDTO foodDTO) {
+        FoodModel foodModel = new FoodModel();
+        foodModel.setFoodId(foodDTO.getId());
+        foodModel.setName(foodDTO.getName());
+        foodModel.setDescription(foodDTO.getDescription());
+        foodModel.setKcal(foodDTO.getKcal());
+        foodModel.setQuantity(foodDTO.getQuantity());
+        foodModel.setFoodType(foodDTO.getFoodType());
+        foodModel.setImgUrl(foodDTO.getImgUrl());
+        foodModel.setMeals(foodDTO.getMeals());
+        return foodModel;
+    }
+
+    public FoodDTO map(FoodModel foodModel) {
+        FoodDTO foodDTO = new FoodDTO();
+        foodDTO.setName(foodModel.getName());
+        foodDTO.setDescription(foodModel.getDescription());
+        foodDTO.setId(foodModel.getFoodId());
+        foodDTO.setKcal(foodModel.getKcal());
+        foodDTO.setQuantity(foodModel.getQuantity());
+        foodDTO.setFoodType(foodModel.getFoodType());
+        foodDTO.setImgUrl(foodModel.getImgUrl());
+        foodDTO.setMeals(foodModel.getMeals());
+        return foodDTO;
+    }
+
 }
